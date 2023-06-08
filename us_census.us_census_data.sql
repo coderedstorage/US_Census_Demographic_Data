4cdf0b57b0ff2a085256cc31ce55167ed2349f60
coderedstorage <130547102+coderedstorage@users.noreply.github.com>
2023-06-08 02:08:45 +0200

    MySQL to generate us_census_data.csv

--- us_census.us_census_data.sql
+++ us_census.us_census_data.sql
@@ -1,26 +1,45 @@
 CREATE OR REPLACE VIEW us_census.us_census_data AS 
 WITH relevant_sates AS
-(SELECT distinct state FROM us_census.metro) 
+(SELECT distinct state FROM us_census.metro),
+US_avg_IncomePerCap AS
+(SELECT SUM(TotalPop*IncomePerCap)/SUM(TotalPop) FROM us_census.kaggle_data),
+state_has_metro AS
+(SELECT DISTINCT state FROM us_census.metro)
+
 SELECT 
     usc.*, 
-    CASE 
+    
+        CASE 
     WHEN mtr.metropolitan IS NOT NULL
     THEN mtr.metropolitan
     ELSE usc.county
-    END metropolitan_or_county,
-  
-      CASE 
-    WHEN mtr.top_10_msa_name IS NOT NULL
-    THEN mtr.top_10_msa_name
-    ELSE "other counties"
-    END top_10_msa,
-  
-      CASE 
-    WHEN mtr.metropolitan IS NOT NULL
-    THEN 'True'
-    ELSE 'False'
-    END top_10_metro
+    END metro_or_county,
     
+	CASE 
+    WHEN mtr.major_msa IS NOT NULL
+    THEN mtr.major_msa
+    ELSE "other counties"
+    END major_msa,
+
+	CASE 
+    WHEN mtr.metropolitan IS NOT NULL
+    THEN 'Y'
+    ELSE 'N'
+    END is_major_metro,
+
+      CASE 
+    WHEN usc.IncomePerCap >= 60000 THEN "$60K+"
+    WHEN usc.IncomePerCap >= 50000 THEN "$50K-$60K"
+    WHEN usc.IncomePerCap >= 40000 THEN "$40K-$50K"
+    WHEN usc.IncomePerCap >= 30000 THEN "$30K-$40K"
+    WHEN usc.IncomePerCap >= (SELECT * FROM US_avg_IncomePerCap) THEN "Avg-$30K"
+    ELSE "Below avg" END income_band_county,
+    
+    CASE WHEN 
+    (SELECT smtr.state FROM state_has_metro smtr WHERE smtr.state = usc.state)
+    IS NULL THEN 'N'
+    ELSE 'Y' END state_has_metro
+
 FROM us_census.kaggle_data usc
 LEFT JOIN us_census.metro mtr
 ON usc.state = mtr.state AND usc.county = mtr.county
