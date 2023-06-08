90af74003dc1160c78159deba40d34f6c435335a
coderedstorage <130547102+coderedstorage@users.noreply.github.com>
2023-06-08 13:24:28 +0200

    MySQL to generate metro_adj.csv

--- us_census.metro_adj.sql
+++ us_census.metro_adj.sql
@@ -1,30 +1,15 @@
-CREATE OR REPLACE VIEW us_census.metro_adj AS 
-WITH relevant_sates AS
-(SELECT distinct state FROM us_census.metro) 
+CREATE OR REPLACE VIEW us_census.metro_adj AS
 SELECT 
     usc.state,
-    
-    CASE 
-    WHEN mtr.metropolitan IS NOT NULL
-    THEN mtr.metropolitan
-    ELSE usc.county
-    END metro_or_county,
-    
-	CASE 
-    WHEN mtr.major_msa IS NOT NULL
-    THEN mtr.major_msa
-    ELSE "other counties"
-    END major_msa,
-    
-	CASE 
-    WHEN mtr.metropolitan IS NOT NULL
-    THEN 'Y'
+    mtr.metropolitan,
+    mtr.major_msa,
+    SUM(TotalPop) metro_Pop,
+    CASE WHEN SUM(TotalPop) >= 200000 
+    THEN 'Y' 
     ELSE 'N'
-    END is_major_metro,
-    
-    SUM(TotalPop) metro_county_Pop
-
+    END metro_above_200K_midsize_or_larger
 FROM us_census.kaggle_data usc
 LEFT JOIN us_census.metro mtr
 ON usc.state = mtr.state AND usc.county = mtr.county
-GROUP BY 1, 2, 3, 4;
+WHERE mtr.major_msa IS NOT NULL
+GROUP BY 1, 2, 3;
